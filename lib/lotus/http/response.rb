ce46399b6c4dd03fa47fdb3a8b9ec7d20812d247
Luca Guidi <me@lucaguidi.com>
2013-09-24 16:04:44 +0200

    Introducing factory for Response

--- lib/lotus/http/response.rb
+++ lib/lotus/http/response.rb
@@ -4,6 +4,15 @@ module Lotus
   module HTTP
     class Response < ::Rack::Response
       attr_reader :action
+      attr_writer :header
+
+      def self.fabricate(response)
+        new(NullAction.new).tap do |r|
+          r.status = response[0] if response[0]
+          r.header = response[1] if response[1]
+          r.body   = response[2] if response[2]
+        end
+      end
 
       def initialize(action)
         super()
@@ -12,6 +21,11 @@ module Lotus
 
       def body=(body)
         super Array(body)
+      end
+
+      private
+      class NullAction
+        def exposures; {}; end
       end
     end
   end
--- test/http/response_test.rb
+++ test/http/response_test.rb
@@ -6,6 +6,24 @@ describe Lotus::HTTP::Response do
     @response = Lotus::HTTP::Response.new(@action)
   end
 
+  describe '.fabricate' do
+    it 'fabricates response from serialized Rack response' do
+      response = Lotus::HTTP::Response.fabricate([412, {'X-Custom' => 'true'}, ['hello!']])
+
+      response.status.must_equal(412)
+      response.header.must_equal({'X-Custom' => 'true'})
+      response.body.must_equal(['hello!'])
+    end
+
+    it 'fabricates response from partial informations' do
+      response = Lotus::HTTP::Response.fabricate([100])
+
+      response.status.must_equal(100)
+      response.header.must_equal({})
+      response.body.must_equal([])
+    end
+  end
+
   it 'inheriths from Rack::Response' do
     @response.must_be_kind_of(::Rack::Response)
   end
