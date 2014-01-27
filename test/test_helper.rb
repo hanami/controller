6f73962a2345df4ea413c1de6a8067192e30d033
Luca Guidi <me@lucaguidi.com>
2014-01-27 10:49:52 +0100

    Enable test coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,21 @@
 require 'rubygems'
 require 'bundler/setup'
+
+if ENV['COVERAGE'] == 'true'
+  require 'simplecov'
+  require 'coveralls'
+
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ]
+
+  SimpleCov.start do
+    command_name 'test'
+    add_filter   'test'
+  end
+end
+
 require 'minitest/autorun'
 $:.unshift 'lib'
 require 'lotus/controller'
