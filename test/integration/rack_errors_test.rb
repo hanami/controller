db3628e963f3aba76f2d13c3d22959b3f8057eed
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2014-06-13 18:32:51 +0200

    Add test for exception with custom #to_s method

--- test/integration/rack_errors_test.rb
+++ test/integration/rack_errors_test.rb
@@ -2,11 +2,17 @@ require 'test_helper'
 require 'lotus/router'
 
 ErrorsRoutes = Lotus::Router.new do
-  get '/without_message', to: 'errors#without_message'
-  get '/with_message',    to: 'errors#with_message'
+  get '/without_message',     to: 'errors#without_message'
+  get '/with_message',        to: 'errors#with_message'
+  get '/with_custom_message', to: 'errors#with_custom_message'
 end
 
-AuthException = Class.new(StandardError)
+AuthException       = Class.new(StandardError)
+CustomAuthException = Class.new(StandardError) do
+  def to_s
+    "AuthException: #{super} :("
+  end
+end
 
 class ErrorsController
   include Lotus::Controller
@@ -20,6 +26,12 @@ class ErrorsController
   action 'WithMessage' do
     def call(params)
       raise AuthException, %q{AuthException: you're not authorized to see this page!}
+    end
+  end
+
+  action 'WithCustomMessage' do
+    def call(params)
+      raise CustomAuthException, 'plz go away!!'
     end
   end
 end
@@ -37,5 +49,10 @@ describe 'Reference exception in rack.errors' do
   it 'adds exception message to rack.errors' do
     response = @app.get('/with_message')
     response.errors.must_equal "AuthException: you're not authorized to see this page!\n"
+  end
+
+  it 'uses exception string representation' do
+    response = @app.get('/with_custom_message')
+    response.errors.must_equal "AuthException: plz go away!! :(\n"
   end
 end
