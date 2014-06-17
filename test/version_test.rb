328153b864b7ff4bdd8c7ef858c1017e48d8d643
Luca Guidi <me@lucaguidi.com>
2014-06-17 15:15:12 +0200

    Bump version to v0.2.0

--- test/version_test.rb
+++ test/version_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 describe Lotus::Controller::VERSION do
   it 'returns the current version' do
-    Lotus::Controller::VERSION.must_equal '0.1.0'
+    Lotus::Controller::VERSION.must_equal '0.2.0'
   end
 end
