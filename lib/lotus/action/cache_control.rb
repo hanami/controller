037013d52b87a179a5b0aa54bc2d2643205073a2
Karl Freeman <karlfreeman@gmail.com>
2014-06-26 19:03:04 +0100

    WIP
    - some cribbing from Sinatra going on for sure.

--- lib/lotus/action/cache_control.rb
+++ lib/lotus/action/cache_control.rb
@@ -2,57 +2,91 @@ module Lotus
   module Action
     # CacheControl type API
     #
-    # @since 0.1.0
+    # @since 0.2.1
     #
-    # @see Lotus::Action::CacheControl::ClassMethods#accept
+    # @see Lotus::Action::CacheControl::ClassMethods#cache_control
+    # @see Lotus::Action::CacheControl::ClassMethods#expires
     module CacheControl
 
+      # The HTTP header for Cache-Control
+      #
+      # @since 0.2.1
+      # @api private
       CACHE_CONTROL         = 'Cache-Control'.freeze
+
+      # The HTTP header for Expires
+      #
+      # @since 0.2.1
+      # @api private
       EXPIRES               = 'Expires'.freeze
-      TRUE                  = 'true'.freeze
-      SETTABLE_DIRECTIVES   = [:max_age, :s_maxage].freeze
-      TRUTHY_DIRECTIVES     = [:public, :private, :no_cache, :no_store, :no_transform, :must_revalidate, :proxy_revalidate].freeze
-      ALL_DIRECTIVES        = (TRUTHY_DIRECTIVES | SETTABLE_DIRECTIVES).freeze
 
-      ALL_DIRECTIVES.each do |d|
+      # Cache-Control directives which have values
+      #
+      # @since 0.2.1
+      # @api private
+      VALUE_DIRECTIVES      = %i(max_age s_maxage).freeze
+
+      # Cache-Control directives which are implicitly true
+      #
+      # @since 0.2.1
+      # @api private
+      NON_VALUE_DIRECTIVES  = %i(public private no_cache no_store no_transform must_revalidate proxy_revalidate).freeze
+
+      (NON_VALUE_DIRECTIVES | VALUE_DIRECTIVES).each do |d|
         const_set(d.upcase, d.to_s.tr('_', '-'))
-      end
-
-      def self.included(base)
-        base.extend ClassMethods
-      end
-
-      module ClassMethods
-        protected
-        
-        # def cache_control(*values)
-        #   cache_control(*values)
-        # end
-
-        # def expires(amount, *values)
-        #   expires(amount, *args)
-        # end
-
       end
 
       protected
 
+      # Specify response freshness policy for HTTP caches (Cache-Control header).
+      # Any number of non-value directives (:public, :private, :no_cache,
+      # :no_store, :must_revalidate, :proxy_revalidate) may be passed along with
+      # a Hash of value directives (:max_age, :min_stale, :s_max_age).
+      #
+      # See RFC 2616 / 14.9 for more on standard cache control directives:
+      # http://tools.ietf.org/html/rfc2616#section-14.9.1
+      #
+      # @since 0.2.1
+      # @api public
+      #
+      # @example
+      #   require 'lotus/controller'
+      #   require 'lotus/action/cache_control'
+      #
+      #   class Show
+      #     include Lotus::Action
+      #     include Lotus::Action::CacheControl
+      #
+      #     def call(params)
+      #       # ...
+      #
+      #       # set Cache-Control directives
+      #       cache_control :public, max_age: 900, s_maxage: 86400
+      #
+      #       # overwrite previous Cache-Control directives
+      #       cache_control :private, :no_cache, :no_store
+      #       
+      #       => Cache-Control: private, no-store, max-age=900
+      #
+      #     end
+      #   end
+      #
       def cache_control(*values)
         cc_directives = {}
 
-        (cache_control_segments(headers[CACHE_CONTROL]) | cache_control_values(*values)).each do |segment|
+        cache_control_values(*values).each do |segment|
           directive, argument = segment.split('=', 2)
           cc_directives[directive.tr('-', '_').to_sym] = argument || true
         end
 
-        if !cc_directives.empty?
+        unless cc_directives.empty?
           cc_header = []
           cc_directives.delete(:public) if cc_directives.key? :private
           cc_directives.delete(:private) if cc_directives.key? :public
           cc_directives.each do |k, v|
-            if SETTABLE_DIRECTIVES.include?(k)
+            if VALUE_DIRECTIVES.include?(k)
               cc_header << "#{CacheControl.const_get(k.upcase)}=#{v.to_i}"
-            elsif TRUTHY_DIRECTIVES.include?(k) && v == TRUE
+            elsif NON_VALUE_DIRECTIVES.include?(k) && Lotus::Utils::Kernel.Boolean(v)
               cc_header << CacheControl.const_get(k.upcase)
             end
           end
@@ -60,6 +94,37 @@ module Lotus
         end
       end
 
+      # Set the Expires header and Cache-Control/max-age directive. Amount
+      # can be an integer number of seconds in the future or a Time object
+      # indicating when the response should be considered "stale". The remaining
+      # "values" arguments are passed to the #cache_control helper:
+      #
+      # @since 0.2.1
+      # @api public
+      #
+      # @example
+      #   require 'lotus/controller'
+      #   require 'lotus/action/cache_control'
+      #
+      #   class Show
+      #     include Lotus::Action
+      #     include Lotus::Action::CacheControl
+      #
+      #     def call(params)
+      #       # ...
+      #
+      #       # set Cache-Control directives and Expires
+      #       expires 900, :public
+      #
+      #       # overwrite Cache-Control directives and Expires
+      #       expires 300, :private, :no_cache, :no_store
+      #       
+      #       => Expires: Thu, 26 Jun 2014 12:00:00 GMT
+      #       => Cache-Control: private, no-cache, no-store max-age=300
+      #
+      #     end
+      #   end
+      #
       def expires(amount, *values)
         if amount.is_a? Integer
           time    = Time.now + amount.to_i
@@ -69,9 +134,10 @@ module Lotus
           max_age = time - Time.now
         end
 
-        values << { max_age: max_age }
+        directives = *values
+        directives << { max_age: max_age }
         headers.merge!(EXPIRES => time.httpdate)
-        cache_control(*values)
+        cache_control(*directives)
       end
 
       private
@@ -80,21 +146,15 @@ module Lotus
         cache_control_values = []
         values.each do |value|
           if value.is_a? Hash
-            cache_control_values.concat value.map { |k, v|
+            cache_control_values.concat(value.map { |k, v|
               argument = v.is_a?(Time) ? v - Time.now : v
               "#{CacheControl.const_get(k.upcase)}=#{argument}"
-            }
+            })
           elsif value.is_a? Symbol
-            cache_control_values << "#{value}=#{TRUE}"
+            cache_control_values << "#{value}=true"
           end
         end
         cache_control_values
-      end
-
-      def cache_control_segments(header = nil)
-        segments = []
-        segments.concat header.delete(' ').split(',') unless header.nil?
-        segments
       end
 
     end
--- test/integration/cache_control_test.rb
+++ test/integration/cache_control_test.rb
@@ -2,26 +2,63 @@ require 'test_helper'
 require 'lotus/router'
 
 CacheControlRoutes = Lotus::Router.new do
-  get '/cache_control/public',  to: 'cache_control#public'
-  get '/expires/public',  to: 'expires#public'
+  get '/symbol',               to: 'cache_control#symbol'
+  get '/symbols',              to: 'cache_control#symbols'
+  get '/hash',                 to: 'cache_control#hash'
+  get '/hash-containing-time', to: 'cache_control#hash_containing_time'
+end
+
+ExpiresRoutes = Lotus::Router.new do
+  get '/symbol',               to: 'expires#symbol'
+  get '/symbols',              to: 'expires#symbols'
+  get '/hash',                 to: 'expires#hash'
+  get '/hash-containing-time', to: 'expires#hash_containing_time'
 end
 
 class CacheControlController
   include Lotus::Controller
-  action 'Public' do
-
+  action 'Symbol' do
     def call(params)
-      cache_control :public
+      cache_control :private
+    end
+  end
+  action 'Symbols' do
+    def call(params)
+      cache_control :private, :no_cache, :no_store
+    end
+  end
+  action 'Hash' do
+    def call(params)
+      cache_control :public, :no_store, max_age: 900, s_maxage: 86400
+    end
+  end
+  action 'HashContainingTime' do
+    def call(params)
+      cache_control :public, :no_store, max_age: (Time.now + 900), s_maxage: (Time.now + 86400)
     end
   end
 end
 
 class ExpiresController
   include Lotus::Controller
-  action 'Public' do
-
+  action 'Symbol' do
     def call(params)
-      expires 900, :public
+      expires 900, :private
+    end
+  end
+  action 'Symbols' do
+    def call(params)
+      expires 900, :private, :no_cache, :no_store
+    end
+  end
+  action 'Hash' do
+    def call(params)
+      expires 900, :public, :no_store, s_maxage: 86400
+    end
+  end
+  action 'HashContainingTime' do
+    def call(params)
+      expires (Time.now + 900), :public, :no_store, s_maxage: (Time.now + 86400)
     end
   end
 end
@@ -30,16 +67,58 @@ describe 'Cache control' do
   before do
     @app = Rack::MockRequest.new(CacheControlRoutes)
   end
-
-  it 'returns public cache-control headers' do
-    response = @app.get('/cache_control/public')
-    response.headers['Cache-Control'].split(', ').must_include('public')
+  it 'accepts a Symbol' do
+    response = @app.get('/symbol')
+    response.headers.fetch('Cache-Control').must_equal('private')
   end
+  it 'accepts multiple Symbols' do
+    response = @app.get('/symbols')
+    response.headers.fetch('Cache-Control').split(', ').must_equal %w(private no-cache no-store)
+  end
+  it 'accepts a Hash' do
+    Time.stub(:now, Time.now) do
+      response = @app.get('/hash')
+      response.headers.fetch('Cache-Control').split(', ').must_equal %w(public no-store max-age=900 s-maxage=86400)
+    end
+  end
+  it 'accepts a Hash containing Time objects' do
+    Time.stub(:now, Time.now) do
+      response = @app.get('/hash-containing-time')
+      response.headers.fetch('Cache-Control').split(', ').must_equal %w(public no-store max-age=900 s-maxage=86400)
+    end
+  end
+end
 
-  it 'returns public cache-control as well as expires headers' do
-    response = @app.get('/expires/public')
-    # Timecop
-    response.headers['Expires'].must_be_kind_of String
-    response.headers['Cache-Control'].split(', ').must_include('public')
+describe 'Expires' do
+  before do
+    @app = Rack::MockRequest.new(ExpiresRoutes)
+  end
+  it 'accepts a Symbol' do
+    Time.stub(:now, Time.now) do
+      response = @app.get('/symbol')
+      response.headers.fetch('Expires').must_equal (Time.now + 900).httpdate
+      response.headers.fetch('Cache-Control').split(', ').must_equal %w(private max-age=900)
+    end
+  end
+  it 'accepts multiple Symbols' do
+    Time.stub(:now, Time.now) do
+      response = @app.get('/symbols')
+      response.headers.fetch('Expires').must_equal (Time.now + 900).httpdate
+      response.headers.fetch('Cache-Control').split(', ').must_equal %w(private no-cache no-store max-age=900)
+    end
+  end
+  it 'accepts a Hash' do
+    Time.stub(:now, Time.now) do
+      response = @app.get('/hash')
+      response.headers.fetch('Expires').must_equal (Time.now + 900).httpdate
+      response.headers.fetch('Cache-Control').split(', ').must_equal %w(public no-store s-maxage=86400 max-age=900)
+    end
+  end
+  it 'accepts a Hash containing Time objects' do
+    Time.stub(:now, Time.now) do
+      response = @app.get('/hash-containing-time')
+      response.headers.fetch('Expires').must_equal (Time.now + 900).httpdate
+      response.headers.fetch('Cache-Control').split(', ').must_equal %w(public no-store s-maxage=86400 max-age=900)
+    end
   end
 end
