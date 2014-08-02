1e3052e208b3ded75e76df368858a6f4e3a62fd8
Lucas Souza <lucasas@gmail.com>
2014-08-02 16:26:36 -0300

    WIP: LastModified header

--- lib/lotus/action/cache_control/conditional_get.rb
+++ lib/lotus/action/cache_control/conditional_get.rb
@@ -11,17 +11,55 @@ module Lotus
       # @api private
       ETAG          = 'ETag'.freeze
 
-      class ConditionalGet
-        def initialize(env, options)
-          @env, @etag = env, options[:etag]
+      # @since 0.2.1
+      # @api private
+      IF_MODIFIED_SINCE = 'IF_MODIFIED_SINCE'.freeze
+
+      # The HTTP header for Last-Modified
+      #
+      # @since 0.2.1
+      # @api private
+      LAST_MODIFIED = 'Last-Modified'.freeze
+
+      class ETag
+        def initialize(env, value)
+          @env, @value = env, value
         end
 
         def fresh?
-          yield if @etag == @env[IF_NONE_MATCH]
+          @env[IF_NONE_MATCH] && @value == @env[IF_NONE_MATCH]
+        end
+
+        def header
+          { ETAG => @value } if fresh?
+        end
+      end
+
+      class LastModified
+        def initialize(env, value)
+          @env, @value = env, value
+        end
+
+        def fresh?
+          @env[IF_MODIFIED_SINCE] && @value.to_i >= Time.httpdate(@env[IF_MODIFIED_SINCE]).to_i
+        end
+
+        def header
+          { LAST_MODIFIED => @value } if fresh?
+        end
+      end
+
+      class ConditionalGet
+        def initialize(env, options)
+          @validations = [ ETag.new(env, options[:etag]), LastModified.new(env, options[:last_modified]) ]
+        end
+
+        def fresh?
+          yield if @validations.any? &:fresh?
         end
 
         def headers
-          { ETAG => @etag }
+          @validations.map &:header
         end
       end
     end
--- test/integration/cache_control_test.rb
+++ test/integration/cache_control_test.rb
@@ -211,4 +211,45 @@ describe 'Fresh' do
       end
     end
   end
+
+  describe 'last_modified' do
+    describe 'when last modified is less than or equal to IF_MODIFIED_SINCE header' do
+      before { @modified_since = Time.new(2014, 1, 8, 0, 0, 0) }
+
+      it 'halts 304 not modified' do
+        Time.stub(:now, @modified_since) do
+          response = @app.get('/last-modified', {'IF_MODIFIED_SINCE' => @modified_since.httpdate})
+          response.status.must_equal 304
+        end
+      end
+
+      it 'keeps the same IfModifiedSince header' do
+        Time.stub(:now, @modified_since) do
+          response = @app.get('/last-modified', {'IF_MODIFIED_SINCE' => @modified_since.httpdate})
+          response.headers.fetch('Last-Modified').must_equal @modified_since.httpdate
+        end
+      end
+    end
+
+    describe 'when last modified is bigger than IF_MODIFIED_SINCE header' do
+      before do
+        @last_modified = Time.new(2014, 1, 8, 0, 0, 0)
+        @now           = Time.new(2014, 2, 8, 0, 0, 0)
+      end
+
+      it 'completes request' do
+        Time.stub(:now, @last_modified) do
+          response = @app.get('/last-modified', {'IF_MODIFIED_SINCE' => @last_modified.httpdate})
+          response.status.must_equal 200
+        end
+      end
+
+      it 'returns etag header' do
+        Time.stub(:now, @last_modified) do
+          response = @app.get('/last-modified', {'IF_MODIFIED_SINCE' => @last_modified.httpdate})
+          response.headers.fetch('Last-Modified').must_equal @last_modified.httpdate
+        end
+      end
+    end
+  end
 end
