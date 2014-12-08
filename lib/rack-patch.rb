15f759428af1261ede4547ee0179a7861626c2c8
Luca Guidi <me@lucaguidi.com>
2014-12-08 09:50:21 +0100

    Fix lib/rack-patch.rb for MRI 2.2.0 and Rbx 2.3.0 on Ubuntu

--- lib/rack-patch.rb
+++ lib/rack-patch.rb
@@ -1,5 +1,6 @@
 # see https://github.com/rack/rack/pull/659
 require 'rack'
+require 'lotus/utils'
 
 if Rack.release <= '1.5'
   require 'rack/utils'
@@ -8,11 +9,16 @@ if Rack.release <= '1.5'
     def self.best_q_match(q_value_header, available_mimes)
       values = q_values(q_value_header)
 
-      value = values.map do |req_mime, quality|
+      values = values.map do |req_mime, quality|
         match = available_mimes.find { |am| Rack::Mime.match?(am, req_mime) }
         next unless match
         [match, quality]
-      end.compact.sort_by do |match, quality|
+      end.compact
+
+      # See https://github.com/lotus/controller/issues/59
+      values = values.reverse if RUBY_VERSION >= '2.2.0' || Lotus::Utils.rubinius?
+
+      value  = values.sort_by do |match, quality|
         (match.split('/', 2).count('*') * -10) + quality
       end.last
 
