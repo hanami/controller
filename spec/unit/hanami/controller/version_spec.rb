35550a7421f7d2e74d45dd15655e5c6a8b364ba1
Luca Guidi <me@lucaguidi.com>
2017-10-03 11:34:50 +0200

    Prepare for v1.1.0.beta2

--- spec/unit/hanami/controller/version_spec.rb
+++ spec/unit/hanami/controller/version_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe "Hanami::Controller::VERSION" do
   it "returns current version" do
-    expect(Hanami::Controller::VERSION).to eq("1.1.0.beta1")
+    expect(Hanami::Controller::VERSION).to eq("1.1.0.beta2")
   end
 end
