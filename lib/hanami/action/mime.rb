0c3ef5941fe8fecac89f8ccaf11ce6f8d099e2ca
Sergey Fedorov <oni.strech@gmail.com>
2017-12-28 10:34:38 +0100

    Change @since tag to be x.x.x

--- lib/hanami/action/mime.rb
+++ lib/hanami/action/mime.rb
@@ -18,7 +18,7 @@ module Hanami
 
       # The key that returns content mime type from the Rack env
       #
-      # @since 1.1.1
+      # @since x.x.x
       # @api private
       HTTP_CONTENT_TYPE    = 'CONTENT_TYPE'.freeze
 
@@ -176,7 +176,7 @@ module Hanami
         #
         # @param formats[Array<Symbol>] one or more symbols representing mime type(s)
         #
-        # @since 1.1.1
+        # @since x.x.x
         #
         # @example
         #   require 'hanami/controller'
