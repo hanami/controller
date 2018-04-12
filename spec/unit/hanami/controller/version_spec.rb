a451c4bf5a87b5c146697f944a0b443c1cf03c84
Luca Guidi <me@lucaguidi.com>
2018-04-12 09:23:28 +0200

    Bump version to v1.3.0.beta1

--- spec/unit/hanami/controller/version_spec.rb
+++ spec/unit/hanami/controller/version_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe "Hanami::Controller::VERSION" do
   it "returns current version" do
-    expect(Hanami::Controller::VERSION).to eq("1.2.0")
+    expect(Hanami::Controller::VERSION).to eq("1.3.0.beta1")
   end
 end
