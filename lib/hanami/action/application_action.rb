a1e02f480a33a7001fccbd1efc373677037385be
Tim Riley <tim@openmonkey.com>
2020-06-01 23:02:44 +1000

    Move most methods to plain InstanceMethods module

--- lib/hanami/action/application_action.rb
+++ lib/hanami/action/application_action.rb
@@ -11,7 +11,10 @@ module Hanami
         @application = provider.respond_to?(:application) ? provider.application : Hanami.application
 
         define_initialize
-        define_view_integration_methods
+      end
+
+      def included(klass)
+        klass.include InstanceMethods
       end
 
       def inspect
@@ -29,28 +32,6 @@ module Hanami
         end
       end
 
-      def define_view_integration_methods
-        define_method :build_response do |**options|
-          options = options.merge(view_options: method(:view_options))
-          super(**options)
-        end
-        private :build_response
-
-        define_method :view_context do
-          @view_context
-        end
-
-        define_method :view_options do |req, res|
-          {context: view_context&.with(view_context_options(req, res))}.compact
-        end
-        private :view_options
-
-        define_method :view_context_options do |req, res|
-          {request: req, response: res}
-        end
-        private :view_context_options
-      end
-
       def resolve_view_context
         # TODO: make identifier configurable
         identifier = "view.context"
@@ -59,6 +40,27 @@ module Hanami
           provider[identifier]
         elsif application.key?(identifier)
           application[identifier]
+        end
+      end
+
+      module InstanceMethods
+        define_method :view_context do
+          @view_context
+        end
+
+        private
+
+        def build_response(**options)
+          options = options.merge(view_options: method(:view_options))
+          super(**options)
+        end
+
+        def view_options(req, res)
+          {context: view_context&.with(view_context_options(req, res))}.compact
+        end
+
+        def view_context_options(req, res)
+          {request: req, response: res}
         end
       end
     end
