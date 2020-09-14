8529ae119a4985aaa30a45768833a810f64865be
Tim Riley <tim@openmonkey.com>
2020-09-14 21:13:06 +1000

    Tidy comments

--- lib/hanami/action/flash.rb
+++ lib/hanami/action/flash.rb
@@ -3,27 +3,26 @@ require 'hanami/utils/json'
 module Hanami
   class Action
     class Flash < DelegateClass(Hash)
-      # The flash hash for the next request.  This
-      # is what gets written to by #[]=.
+      # The flash hash for the next request, which gets written to by #[]=
       attr_reader :next
 
       # The flash hash for the current request
       alias now __getobj__
 
-      # Setup the next hash when initializing, and handle treat nil
-      # as a new empty hash.
+      # Setup the next hash when initializing, and treat nil as a new empty
+      # hash
       def initialize(hash = {})
-        super(hash||{})
+        super(hash || {})
         @next = {}
       end
 
-      # Update the next hash with the given key and value.
+      # Update the next hash with the given key and value
       def []=(k, v)
         @next[k] = v
       end
 
-      # Remove given key from the next hash, or clear the next hash if
-      # no argument is given.
+      # Remove given key from the next hash, or clear the next hash if no
+      # argument is given
       def discard(key = nil)
         if key.nil?
           @next.clear
@@ -32,9 +31,9 @@ module Hanami
         end
       end
 
-      # Copy the entry with the given key from the current hash to the
-      # next hash, or copy all entries from the current hash to the
-      # next hash if no argument is given.
+      # Copy the entry with the given key from the current hash to the next
+      # hash, or copy all entries from the current hash to the next hash if no
+      # argument is given
       def keep(key = nil)
         if key.nil?
           @next.merge!(self)
@@ -43,7 +42,7 @@ module Hanami
         end
       end
 
-      # Replace the current hash with the next hash and clear the next hash.
+      # Replace the current hash with the next hash and clear the next hash
       def sweep
         replace(@next)
         @next.clear
