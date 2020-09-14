6b0a0a78a3e4c7537fdc6658ceda9831c70c9a65
Tim Riley <tim@openmonkey.com>
2020-09-14 22:20:55 +1000

    Remove flash instance method from Action
    The flash is accessible on the response now, not the action instance (which is stateless and doesn’t keep per-request information)

--- spec/unit/hanami/action/session_spec.rb
+++ spec/unit/hanami/action/session_spec.rb
@@ -21,40 +21,4 @@ RSpec.describe Hanami::Action do
       expect(response.session[:foo]).to eq("bar")
     end
   end
-
-  describe "flash" do
-    # it "exposes flash" do
-    #   action = FlashAction.new
-    #   response = action.call({})
-
-    #   expect(response[:flash][:error]).to eq("ouch")
-    # end
-
-    # describe "#each" do
-    #   it "iterates through data" do
-    #     action = FlashAction.new
-    #     response = action.call({})
-
-    #     result = []
-    #     response[:flash].each do |type, message|
-    #       result << [type, message]
-    #     end
-
-    #     expect(result).to eq([[:error, "ouch"]])
-    #   end
-    # end
-
-    # describe "#map" do
-    #   it "iterates through data" do
-    #     action = FlashAction.new
-    #     response = action.call({})
-
-    #     result = response.flash.map do |type, message|
-    #       [type, message]
-    #     end
-
-    #     expect(result).to eq([[:error, "ouch"]])
-    #   end
-    # end
-  end
 end
