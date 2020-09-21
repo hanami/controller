09ff00cc27ae09cabb7961e5a2f1cbfea828d70d
Tim Riley <tim@openmonkey.com>
2020-09-21 22:11:23 +1000

    Reintroduce CSRFProtection module
    This module disappeared from the hanami repo during the beginnings of the 2.0 overhaul. Adding it back to hanami-controller makes sense, since this is where the CSRF protection will be taking place.
    
    This introduces the module (and corresponding tests), but does not yet automatically enable it if sessions are enabled.

---
+++ spec/unit/hanami/action/csrf_protection_spec.rb
@@ -0,0 +1,114 @@
+# frozen_string_literal: true
+
+require "hanami/action"
+require "hanami/action/csrf_protection"
+
+RSpec.describe Hanami::Action::CSRFProtection do
+  subject(:action) {
+    Class.new(Hanami::Action) {
+      include Hanami::Action::CSRFProtection
+    }.new
+  }
+
+  let(:response) { action.(request) }
+
+  describe "Requests requiring protection (Non-idempotent request methods)" do
+    let(:request) { {"REQUEST_METHOD" => "POST"} }
+
+    context "No existing session" do
+      let(:request) { super().merge("rack.session" => {}) }
+
+      context "non-matching CSRF token in request" do
+        let(:request) { super().merge(_csrf_token: "non-matching") }
+
+        it "rejects the request" do
+          expect { response }.to raise_error Hanami::Action::InvalidCSRFTokenError
+        end
+      end
+
+      context "missing CSRF token in request" do
+        it "rejects the request" do
+          expect { response }.to raise_error Hanami::Action::InvalidCSRFTokenError
+        end
+      end
+    end
+
+    context "Existing session" do
+      let(:request) { super().merge("rack.session" => session) }
+      let(:session) { {_csrf_token: session_token} }
+      let(:session_token) { "abc123" }
+
+      context "matching CSRF token in request" do
+        let(:request) { super().merge(_csrf_token: session_token) }
+
+        it "accepts the request" do
+          expect(response.status).to eq 200
+        end
+      end
+
+      context "non-matching CSRF token in request" do
+        let(:request) { super().merge(_csrf_token: "non-matching") }
+
+        it "rejects the request" do
+          expect { response }.to raise_error Hanami::Action::InvalidCSRFTokenError
+        end
+      end
+
+      context "missing CSRF token in request" do
+        it "rejects the request" do
+          expect { response }.to raise_error Hanami::Action::InvalidCSRFTokenError
+        end
+      end
+
+      context "CSRF checks skipped" do
+        subject(:action) {
+          Class.new(Hanami::Action) {
+            include Hanami::Action::CSRFProtection
+
+            def verify_csrf_token?(req, res)
+              false
+            end
+          }.new
+        }
+
+        context "missing CSRF token in request" do
+          it "accepts the request" do
+            expect(response.status).to eq 200
+          end
+        end
+
+        context "non-matching CSRF token in request" do
+          let(:request) { super().merge(_csrf_token: "non-matching") }
+
+          it "accepts the request" do
+            expect(response.status).to eq 200
+          end
+        end
+      end
+    end
+  end
+
+  context "Requests not requiring protection (Idempotent request methods)" do
+    %w[GET HEAD TRACE OPTIONS].each do |request_method|
+      describe request_method do
+        let(:request) { {"REQUEST_METHOD" => request_method} }
+
+        context "No existing session" do
+          it "sets a CSRF token in the response session" do
+            expect(response.session[:_csrf_token]).to be_a_kind_of String
+          end
+        end
+
+        context "Existing session" do
+          let(:request) { super().merge("rack.session" => session) }
+          let(:session) { {_csrf_token: session_token} }
+          let(:session_token) { "abc123" }
+
+          it "includes the existing CSRF token in the response session" do
+            expect(response.session[:_csrf_token]).to eq session_token
+          end
+        end
+      end
+    end
+  end
+end
