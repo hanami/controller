9db3d6f9255f8983eba6b09d6417f020329dd026
Tim Riley <tim@openmonkey.com>
2020-09-29 22:16:31 +1000

    Enable CSRF protection automatically

--- lib/hanami/action/application_configuration.rb
+++ lib/hanami/action/application_configuration.rb
@@ -8,6 +8,7 @@ module Hanami
     class ApplicationConfiguration
       include Dry::Configurable
 
+      setting :csrf_protection
       setting :name_inference_base, "actions"
       setting :view_context_identifier, "view.context"
       setting :view_name_inferrer, ViewNameInferrer
@@ -17,11 +18,22 @@ module Hanami
         _settings << action_setting.dup
       end
 
-      def initialize(*)
-        super
+      def initialize(application_config, *args)
+        super(*args)
 
+        @application_config = application_config
+
+        # Adjust defaults for base configuration
         config.default_request_format = :html
         config.default_response_format = :html
+      end
+
+      def finalize!
+        # A nil value for `csrf_protection` means it has not been explicitly configured
+        # (neither true nor false), so we can defaut it to whether sessions are enabled
+        if config.csrf_protection.nil?
+          config.csrf_protection = application_config.sessions.enabled?
+        end
       end
 
       # Returns the list of available settings
@@ -35,6 +47,8 @@ module Hanami
       end
 
       private
+
+      attr_reader :application_config
 
       def method_missing(name, *args, &block)
         if config.respond_to?(name)
