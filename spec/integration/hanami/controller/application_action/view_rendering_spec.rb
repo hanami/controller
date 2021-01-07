b684824a2b2932e762ebbc5b97c76906f5fa2d96
Luca Guidi <me@lucaguidi.com>
2021-01-07 10:18:58 +0100

    Ensure rendering inline `options` to take the precedence over `exposures`

--- spec/integration/hanami/controller/application_action/view_rendering_spec.rb
+++ spec/integration/hanami/controller/application_action/view_rendering_spec.rb
@@ -34,7 +34,8 @@ RSpec.describe "View rendering in application actions", :application_integration
 
               def handle(req, res)
                 res[:job] = "Singer"
-                res.render view, name: req.params[:name]
+                res[:age] = 0
+                res.render view, name: req.params[:name], age: 51
               end
             end
           end
@@ -70,7 +71,7 @@ RSpec.describe "View rendering in application actions", :application_integration
         module Main
           module Views
             class TestView < Main::View
-              expose :name, :job
+              expose :name, :job, :age
             end
           end
         end
@@ -87,6 +88,7 @@ RSpec.describe "View rendering in application actions", :application_integration
         - request.params.to_h.values.sort.each do |value|
           p = value
         p = job
+        p = age
       SLIM
 
       require "hanami/init"
@@ -95,7 +97,7 @@ RSpec.describe "View rendering in application actions", :application_integration
       response = action.(name: "Jennifer", last_name: "Lopez")
       rendered = response.body[0]
 
-      expect(rendered).to eq "<html><body><h1>Hello, Jennifer</h1><p>Jennifer</p><p>Lopez</p><p>Singer</p></body></html>"
+      expect(rendered).to eq "<html><body><h1>Hello, Jennifer</h1><p>Jennifer</p><p>Lopez</p><p>Singer</p><p>51</p></body></html>"
     end
   end
 end
