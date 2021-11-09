debe0f48fc616c7c17d6ce1197f40d2bc85067ba
Tim Riley <tim@openmonkey.com>
2021-11-09 23:46:58 +1100

    Prepare for v2.0.0.alpha3

--- lib/hanami/controller/version.rb
+++ lib/hanami/controller/version.rb
@@ -3,6 +3,6 @@ module Hanami
     # Defines the version
     #
     # @since 0.1.0
-    VERSION = '2.0.0.alpha2'.freeze
+    VERSION = '2.0.0.alpha3'.freeze
   end
 end
--- spec/unit/hanami/controller/version_spec.rb
+++ spec/unit/hanami/controller/version_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe "Hanami::Controller::VERSION" do
   it "returns current version" do
-    expect(Hanami::Controller::VERSION).to eq("2.0.0.alpha2")
+    expect(Hanami::Controller::VERSION).to eq("2.0.0.alpha3")
   end
 end
