27a58f143a05206e21c4a0f6df16d9eb7636ee2c
Tim Riley <tim@openmonkey.com>
2021-11-24 11:29:11 +1100

    Make the routes helpr an injectable dependency

--- lib/hanami/action/application_action.rb
+++ lib/hanami/action/application_action.rb
@@ -28,24 +28,16 @@ module Hanami
       def define_initialize(action_class)
         resolve_view = method(:resolve_paired_view)
         resolve_context = method(:resolve_view_context)
+        resolve_routes = method(:resolve_routes)
 
         define_method :initialize do |**deps|
           # Conditionally assign these to repsect any explictly auto-injected
           # dependencies provided by the class
           @view ||= deps[:view] || resolve_view.(self.class)
           @view_context ||= deps[:view_context] || resolve_context.()
+          @routes ||= deps[:routes] || resolve_routes.()
 
           super(**deps)
-        end
-      end
-
-      def resolve_view_context
-        identifier = application.config.actions.view_context_identifier
-
-        if provider.key?(identifier)
-          provider[identifier]
-        elsif application.key?(identifier)
-          application[identifier]
         end
       end
 
@@ -58,6 +50,20 @@ module Hanami
         view_identifiers.detect { |identifier|
           break provider[identifier] if provider.key?(identifier)
         }
+      end
+
+      def resolve_view_context
+        identifier = application.config.actions.view_context_identifier
+
+        if provider.key?(identifier)
+          provider[identifier]
+        elsif application.key?(identifier)
+          application[identifier]
+        end
+      end
+
+      def resolve_routes
+        application[:routes_helper] if application.key?(:routes_helper)
       end
 
       def configure_action(action_class)
@@ -87,6 +93,7 @@ module Hanami
       module InstanceMethods
         attr_reader :view
         attr_reader :view_context
+        attr_reader :routes
 
         def build_response(**options)
           options = options.merge(view_options: method(:view_options))
@@ -104,16 +111,6 @@ module Hanami
         def finish(req, res, halted)
           res.render(view, **req.params) if render?(res)
           super
-        end
-
-        # Access application routes
-        #
-        # @return [Hanami::Application::RoutesHelper]
-        #
-        # @since 2.0.0
-        # @see Hanami::Application::RoutesHelper
-        def routes
-          Hanami.application[:routes_helper]
         end
 
         # Decide whether to render the current response with the associated view.
