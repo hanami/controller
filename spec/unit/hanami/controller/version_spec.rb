92500de53c7c45644f9f95c578e5ea482652a844
Peter Solnica <peter@solnica.online>
2022-11-05 07:39:30 +0100

    Fix version spec

--- spec/unit/hanami/controller/version_spec.rb
+++ spec/unit/hanami/controller/version_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe "Hanami::Controller::VERSION" do
   it "returns current version" do
-    expect(Hanami::Controller::VERSION).to eq("2.0.0.beta4")
+    expect(Hanami::Controller::VERSION).to eq("2.0.0.rc1")
   end
 end
