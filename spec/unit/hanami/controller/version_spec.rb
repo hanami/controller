2816c9014997f359a9699374d59e071c42f50109
Luca Guidi <me@lucaguidi.com>
2022-12-25 12:42:57 +0100

    Ruby 3.2 (#408)

--- spec/unit/hanami/controller/version_spec.rb
+++ spec/unit/hanami/controller/version_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe "Hanami::Controller::VERSION" do
   it "returns current version" do
-    expect(Hanami::Controller::VERSION).to eq("2.0.0")
+    expect(Hanami::Controller::VERSION).to eq("2.0.1")
   end
 end
