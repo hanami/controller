f44fbcea3da0a8a085cb6ce52434c9e247010b2e
Tim Riley <tim@riley.id.au>
2025-09-05 15:23:08 +1000

    Mark failing Rack 3 test as pending, waiting on a fix within rack

--- spec/integration/hanami/controller/sessions_without_cookies_spec.rb
+++ spec/integration/hanami/controller/sessions_without_cookies_spec.rb
@@ -11,13 +11,15 @@ RSpec.describe "Sessions without cookies application" do
     last_response
   end
 
-  xit "Set-Cookie with rack.session value is sent only one time" do
+  it "Set-Cookie with rack.session value is sent only one time" do
     get "/", {}, "HTTP_ACCEPT" => "text/html"
 
     set_cookie_value = response.headers["Set-Cookie"]
     rack_session = /(rack.session=.+);/i.match(set_cookie_value).captures.first.gsub("; path=/", "")
 
     get "/", {}, "HTTP_ACCEPT" => "text/html", "Cookie" => rack_session
+
+    pending "Restore when https://github.com/rack/rack-session/issues/52 is fixed" if Hanami::Action.rack_3?
 
     expect(response.headers).to_not have_key("Set-Cookie")
   end
