b817960a40a7e48710f2563a2312334d98c075d5
Tim Riley <tim@riley.id.au>
2025-10-17 16:37:14 +1100

    Prepare for v2.3.0.beta2

--- spec/unit/hanami/controller/version_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe "Hanami::Controller::VERSION" do
-  it "returns current version" do
-    expect(Hanami::Controller::VERSION).to eq("2.3.0.beta1")
-  end
-end
